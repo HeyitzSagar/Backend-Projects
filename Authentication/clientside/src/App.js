43669c5f644b60b4480ce8baafd044bf1b16ae7a
Sagar Gupta <sagargupta.gola@gmail.com>
2024-04-28 13:53:23 +0530

    User and Signup Form format

--- Authentication/clientside/src/App.js
+++ Authentication/clientside/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Pages/Header";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 const App = () => {
   return (
     <div>
-     <h1>Welcome to the Authentication on the basis of json web token </h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Authentication/clientside/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold font-mono'>This is Home Component</h1>
+    </div>
+  )
+}
+
+export default Home
